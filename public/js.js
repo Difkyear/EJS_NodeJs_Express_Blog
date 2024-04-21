62e28798ffa30dbc08a8a51b3ce38e616c9ab7b6
Difkyear <55550427+Difkyear@users.noreply.github.com>
2024-04-21 12:43:25 +0800

    fix navbar active statement and add customize font

---
+++ public/js.js
@@ -0,0 +1,5 @@
+<script>
+  $(".nav-item .nav-link.active").removeClass("active");
+  document.querySelector("body > div:nth-child(1) > header > ul >
+  li:nth-child(2) > a").classList.add("active")
+</script>;
